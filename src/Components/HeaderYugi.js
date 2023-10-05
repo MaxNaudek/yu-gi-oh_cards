f9ec2be5fa579adafd9646d594dca443657a10fd
Max H <78988911+MaxNaudek@users.noreply.github.com>
2023-10-05 13:05:08 -0600

    ChangeTitle

--- src/Components/HeaderYugi.js
+++ src/Components/HeaderYugi.js
@@ -1,10 +1,13 @@
 /**
  * This function returns a JSX with the main title at the top of the view.
  */
-import '../App.css'
+import '../App.css';
+import { fontYugi } from '../ImagePrototype/fontbolt.png';
 function HeaderYugi() {
   return (
-    <div className="d-flex justify-content-center" style={{fontSize: '4rem', margin: '2%',  border: '1rem double #3B9AE1', fontFamily:'Fira Sans, sans-serif'}}>Yu Gi Oh! Duel Monster Cards!!</div>
+    <div className="d-flex justify-content-center" style={{fontSize: '4rem', margin: '2%',  border: '1rem double #3B9AE1', fontFamily:'Fira Sans, sans-serif'}}>
+     <img alt='yugioh title font' src={fontYugi} />
+    </div>
   )
 }
 
